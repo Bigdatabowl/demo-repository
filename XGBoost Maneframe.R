539cbcd639227da8a1cd57957113f17bc491f0b3
TheDhakKnight <145599062+TheDhakKnight@users.noreply.github.com>
2023-12-28 12:11:57 -0600

    Add files via upload

---
+++ XGBoost Maneframe.R
@@ -0,0 +1,76 @@
+library(tidyverse)
+library(ggplot2)
+library(gganimate)
+library(nflverse)
+library(tidymodels)
+library(stacks)
+
+plays <- read.csv('plays.csv')
+final_data <- readRDS('final_data.RDS')
+
+yards <- plays %>% 
+  select(gameId, playId, playResult, passResult)
+
+final_data <- final_data %>% 
+  filter(week > 1) %>% 
+  left_join(yards) %>% 
+  mutate(offenseFormation = as.factor(offenseFormation),
+         down = as.factor(down)) 
+rushing_data <- final_data %>% filter(passResult == '')
+passing_data <- final_data %>% filter(passResult != '')
+
+
+
+####
+#Modeling
+####
+
+nfl_split <- make_splits(
+  rushing_data %>% filter(week <= 7),
+  rushing_data %>% filter(week > 7))
+
+
+
+nfl_training <- training(nfl_split)
+nfl_test <- testing(nfl_split)
+
+nfl_recipe <- recipe(playResult ~ offenseFormation + down + defendersInTheBox + 
+                       yardsToGo + yardstoEnd + Scorediff + run_yards_per_attempt_off + 
+                       run_yards_per_attempt_def, data = nfl_training) %>% 
+  step_dummy(all_nominal_predictors())
+
+nfl_prep <- prep(nfl_recipe)
+nfl_juice <- juice(nfl_prep)
+
+
+### XGBOOST
+nfl_xgb <- boost_tree(
+  mode = 'regression',
+  mtry = tune(),
+  trees = tune(),
+  min_n = tune(),
+  tree_depth = tune(),
+  learn_rate = tune(),
+  loss_reduction = tune(),
+  sample_size = tune(),
+  stop_iter = tune()) %>% 
+  set_engine("xgboost")
+
+nfl_xgb_param <- extract_parameter_set_dials(nfl_xgb) %>% 
+  update(mtry = mtry(c(1, 14)))
+
+xgboost_grid <- grid_max_entropy(
+  nfl_xgb_param, size = 16)
+
+nfl_xgb_wf <- workflow() %>% 
+  add_recipe(nfl_recipe) %>% 
+  add_model(nfl_xgb)
+
+nfl_xgb_tune_results <- tune_grid(
+  nfl_xgb_wf,
+  resamples = tidy_kfolds,
+  grid = xgboost_grid,
+  metrics = metric_set(rmse)
+)
+
+saveRDS(nfl_xgb_tune_results, "nfl_xgb_tune_results.rds")
